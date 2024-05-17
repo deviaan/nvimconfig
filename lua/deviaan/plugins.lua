3bfd72c53378641b3b56f7350dcd97a4cfccdc52
Juan Valdivia <deviaan2@gmail.com>
2024-05-17 11:09:22 -0600

    remove norg

--- lua/deviaan/plugins.lua
+++ lua/deviaan/plugins.lua
@@ -83,29 +83,6 @@ return require('packer').startup(function(use)
     -- Status line
     use 'beauwilliams/statusline.lua'
 
-    -- Neorg
-    use {
-        "nvim-neorg/neorg",
-        config = function()
-            require('neorg').setup {
-                load = {
-                    ["core.defaults"] = {}, -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                            },
-                        },
-                    },
-                },
-            }
-        end,
-        run = ":Neorg sync-parsers",
-        requires = "nvim-lua/plenary.nvim",
-        tag = "*",
-    }
-
     -- Git integration
     use {
         'tanvirtin/vgit.nvim',
